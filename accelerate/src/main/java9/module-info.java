857241caec567302e4c14b9181a9e6777d247cf6
Dragan <dragan@Dragans-MacBook-Pro.local>
2025-04-01 13:39:16 +0200

    Initial working POC.

---
+++ accelerate/src/main/java9/module-info.java
@@ -0,0 +1,5 @@
+module uncomplicate.javacpp.accelerate {
+    requires transitive org.bytedeco.javacpp;
+    exports uncomplicate.javacpp.accelerate.global;
+    exports uncomplicate.javacpp.accelerate.presets;
+}
